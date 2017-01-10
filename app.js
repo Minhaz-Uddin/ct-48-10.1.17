361278f1c2f8d1990365c82a7e17dbc69dff6227
Md. Minhaz Faisal <faisalahmed01944@gmail.com>
2017-01-10 13:31:19 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// $("#demo").click(function(){
+// 	$("#tag").text("Hey there");
+// });
+
+// $("#demo").click(function(){
+// 	$("#tag1").text("World");
+// 	$("#tag2").text("Hello");
+// })
+
+// $("#demo").click(function(){
+// 	var a= $("#tag1").text();
+// 	$("#tag2").text(a);
+// });
+
